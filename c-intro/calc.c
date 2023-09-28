3e4481abb703995e24c0eb46d7a7559db017b8ba
bothaim <145869050+bothaim@users.noreply.github.com>
2023-09-28 07:07:35 +0000

    code to sum 2 values

---
+++ c-intro/calc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a;
+    int b;
+    printf("Enter A:");
+    scanf("%i", &a);
+    printf("Enter B:");
+    scanf("%i", &b);
+    printf("The sum of a + b = %i\n", a + b);
+    return 0;
+}
