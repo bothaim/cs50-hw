644515d7b0f35be645fc17b34dabf4fa9b2785d3
bothaim <145869050+bothaim@users.noreply.github.com>
2023-09-28 20:10:21 +0000

    debug removal

--- c-intro/condition.c
+++ c-intro/condition.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void){
 
     const int VALUE = 10;
     int answer;
-    printf("Enter the number: ");
-    scanf("%i", &answer);
+    char c = ' ';
+    int a;
 
-    if (answer > VALUE){
-        printf("Your number %i is higher to coded value: %i \n", answer, VALUE);
-        if (answer % 2 == 0){
-            printf("The number is even the reminder is: %i \n", answer % 2);
+    while (c != 'y' && c != 'Y'){
+        printf("Enter the number: ");
+        scanf("%i", &answer);
+        if (answer > VALUE){
+            printf("Your number %i is higher to coded value: %i \n", answer, VALUE);
+            if (answer % 2 == 0){
+                printf("The number is even the reminder is: %i \n", answer % 2);
+                }
+            else{
+                printf("The number is odd the reminder is: %i \n", answer % 2);
             }
-        else{
-            printf("The number is odd the reminder is: %i \n", answer % 2);
         }
-    }
-    else if (answer < VALUE){
-        printf("Your number %i is lower to coded value: %i \n", answer, VALUE);
-        if (answer % 2 == 0){
-            printf("The number is even the reminder is: %i \n", answer % 2);
-        }
-        else{
-            printf("The number is odd the reminder is: %i \n", answer % 2);
-        }
-    }
-    else{
-        printf("Your number %i is equal to coded value: %i \n", answer, VALUE);
-        if (answer % 2 == 0){
-            printf("The number is even the reminder is: %i \n", answer % 2);
+        else if (answer < VALUE){
+            printf("Your number %i is lower to coded value: %i \n", answer, VALUE);
+            if (answer % 2 == 0){
+                printf("The number is even the reminder is: %i \n", answer % 2);
+            }
+            else{
+                printf("The number is odd the reminder is: %i \n", answer % 2);
+            }
         }
         else{
-            printf("The number is odd the reminder is: %i \n", answer % 2);
+            printf("Your number %i is equal to coded value: %i \n", answer, VALUE);
+            if (answer % 2 == 0){
+                printf("The number is even the reminder is: %i \n", answer % 2);
+            }
+            else{
+                printf("The number is odd the reminder is: %i \n", answer % 2);
+            }
         }
+        printf("Would you like to exit Y/N: ");
+        scanf(" %c", &c);
+        //c = tolower(c);
     }
     return 0;
 }
