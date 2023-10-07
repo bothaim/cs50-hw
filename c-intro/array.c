e002624a744d46355b7af09493448d82bb5aa03d
bothaim <145869050+bothaim@users.noreply.github.com>
2023-10-07 11:44:39 +0000

    Simple code to show array functionality

--- c-intro/array.c
+++ c-intro/array.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void my_array(void);
+int my_random_int(void);
+int my_array_size(long full_array, long array_clust);
 
 int main(void)
 {
-    int i_arry [20];
-    printf("Size of array - %li, size of array cluster %li \n", sizeof(i_arry), sizeof(i_arry[0]));
+    my_array();
 
+}
+
+void my_array(void)
+{
+    int i_array_size = my_random_int();
+    int a_temp [i_array_size];
+    int i_length = my_array_size(sizeof(a_temp), sizeof(a_temp[0]));
+    int i_temp;
+    printf("The size of array - %li, the lize of cluster - %li, the aray length - %i\n", sizeof(a_temp), sizeof(a_temp[0]), i_length);
+    for (int i = 0; i < i_length; i++)
+    {
+        i_temp = my_random_int();
+        a_temp[i] = i_temp;
+    }
+    for (int j = 0; j < i_length; j++)
+    {
+        printf("a_temp[%i] - %i\n", j, a_temp[j]);
+    }
+    
+}
+
+int my_random_int(void)
+{
+    // srand(time(NULL));
+    return rand() % 100;
+}
+
+int my_array_size(long full_array, long array_clust)
+{
+    return full_array / array_clust;
 }
