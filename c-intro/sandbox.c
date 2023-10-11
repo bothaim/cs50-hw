74a37dac9f12dd1617858309a10f8ff32d69d048
haimpau <haimpau@gmail.com>
2023-10-11 16:52:17 +0300

    Sandbox - Merge reached the lowest level of RS

--- c-intro/sandbox.c
+++ c-intro/sandbox.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-int f_sort_left(int start, int end);
+int sort(int *arr, int start, int end);
 
 int main(void)
 {
     int i_arr[]= {3,9,2,0,4,7};
     int i_size = sizeof(i_arr) / sizeof(i_arr[0]);
     printf("The size of array - %i\n", i_size);
-
-    int end = i_size - ceil(i_size / 2.0);
-    printf("Right half of the array = start - 0, end - %i\n", end);
-    f_sort_left(0, end);
+    sort(i_arr, 0, i_size);
 }
 
-int f_sort_left(int start, int end)
-{
+int sort(int *arr, int start, int end)
+{   
+    //Find right
+    printf("Recived size = start - %i, end - %i\n", start, end);
+    end -= ceil(end / 2.0);
     if (end == start)
     {
-        return 0;
+        printf("You reached the size %i array\n", end);
+        return end;
     }
     else
     {
-        end -= ceil(end / 2.0);
-        printf("Now end - %i\n", end);
-        f_sort_left(start, end);
-        return 1;
+        sort(arr, start, end);
     }
-    printf("Is this printed\n");
+    printf("Recursion finished start - %i, end - %i\n", start, end);
 }
