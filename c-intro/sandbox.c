736f16c6b443354188e172bdecb57b8b68d1eb17
svirpav <pavel.svirida@jotautomation.com>
2023-10-15 23:45:26 +0300

    Sandbox - merge save 15.01.2023

--- c-intro/sandbox.c
+++ c-intro/sandbox.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int sort(int *arr, int start, int end);
+void sort(int *arr, int start, int end);
+void merge(int *arr, int start, int end, int mid);
 
 int main(void)
 {
@@ -11,18 +12,66 @@ int main(void)
     sort(i_arr, 0, i_size - 1);
 }
 
-int sort(int *arr, int start, int end)
+void sort(int *arr, int start, int end)
 {   
     if (start >= end)
     {
-        printf("Base state reached start = %i, end=%i\n");
-        return 0;
+        // printf("Base state reached start = %i, end=%i\n", start, end);
+        return;
     }
     int mid = 0;
     mid = floor((start + end)/ 2.0);
-    printf("Divide starting at positions start = %i, end = %i, mid = %i\n", start, end, mid);
     sort(arr, start, mid);
     sort(arr, mid+1, end);
-    printf("Division Completed.\n");
-    printf("Merge can be started in range %i - %i, with mid = %i\n", start, end, mid);
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+    merge(arr, start, end, mid);
+}
+
+void merge(int *arr, int start, int end, int mid)
+{   
+    int temp_size = (end - start) + 1;
+    int temp_a [temp_size];
+    int pos = 0;
+    int int_start = start;
+    int int_mid = mid + 1;
+    printf("Merge started at start - %i, end - %i, where mid - %i\n", start, end, mid);
+    while (int_start <= mid || int_mid <= end)
+    {
+        if (int_start > mid)
+        {
+            temp_a[pos] = arr[int_mid];
+            int_mid++;
+            pos++;
+        }
+        else if (int_mid > end)
+        {
+            temp_a[pos] = arr[int_start];
+            int_start++;
+            pos++;           
+        }
+        else
+        {
+            if (arr[int_start] < arr[int_mid])
+            {
+                temp_a[pos] = arr[int_start];
+                int_start++;
+                pos++;
+            }
+            else if (arr[int_start] > arr[int_mid])
+            {
+                temp_a[pos] = arr[int_mid];
+                int_mid++;
+                pos++;
+            }       
+        }        
+    }
+    for (int i = start; i < end + 1; i++)
+    {
+        arr[i] = temp_a[i];
+    }
+    
 }
