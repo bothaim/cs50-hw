b0e5bbe63747e1bb853970b4e5be6ec98fde80eb
haimpau <haimpau@gmail.com>
2023-10-17 00:17:10 +0300

    Sotring - Start to implement merge algoritm

--- c-intro/sorting.c
+++ c-intro/sorting.c
@@ -9,8 +9,6 @@ int get_int(void);
 int get_random_int(int size);
 int in_array(int *arr, size_t size, int value);
 void zero_array(int *array, size_t size);
-void sort(int *arr, int size);
-int sort_left(int *arr, int start, int end);
 
 int main(void)
 {
@@ -27,7 +25,7 @@ void run(void)
     // Ask user for array size
     size = get_int();
 
-    //Dynamic array declaration.
+    //User array declaration.
     int i_array[size];
     zero_array(i_array, size);
     while (i_count < size)
@@ -84,25 +82,4 @@ int in_array(int *arr, size_t size, int value)
         }
     }
     return 0;
-}
-
-void sort(int *arr, int size)
-{
-    sort_left(arr, 0, size - ceil(size/2));
-}
-
-int sort_left(int *arr, int start, int end)
-{
-    double i_temp;
-    if (start == end)
-    {
-        return 0;
-    }
-    else
-    {   
-        i_temp = end / 2.0;
-        end -= ceil(i_temp);
-        sort_left(arr, start, end);
-    }
-    printf("START{%i} - END{%i}\n", start, end);
 }
