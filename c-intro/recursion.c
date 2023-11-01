b7fa9f2fe89d1ad648cc0679192ba1ff3b51b532
bothaim <haimpau@gmail.com>
2023-11-01 21:10:15 +0000

    Recursion - Factorials comparision

--- c-intro/recursion.c
+++ c-intro/recursion.c
@@ -10,12 +10,28 @@ int main(void)
     long result;
 
     value = get_int("Please enter the number");
+    long results[value];
+    int times;
     printf("You recived - %i\n", value);
     for (int i = 1; i <= value; i++)
     {
-        result = get_factorial(i);
-        printf("Factorial of %i\t= %li\n", i, result);
+        results[i-1] = get_factorial(i);
+        // printf("Factorial of %i\t= %li\n", i, result);
     }
+
+    for (int i = 0; i < value; i++)
+    {   
+        if (i == 0)
+        {
+            printf("Factorial of %i\t= %li\t| Intial factorial\n", i+1, results[i]);
+        }
+        else
+        {   
+            times = results[i] / results[i-1];
+            printf("Factorial of %i\t= %li\t| times\t %i\n", i+1, results[i], times);
+        }
+    }
+    
 }
 
 int get_int(char *promt)
